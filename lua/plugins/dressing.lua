8c1c4b26468c0ca7378109d3a0f3a90f79c7decb
Eliezer Ambion <ambion@theoriamedical.com>
2023-07-09 23:02:26 +0800

    Tweak dressing

--- lua/plugins/dressing.lua
+++ lua/plugins/dressing.lua
@@ -2,23 +2,30 @@
 return {
   'stevearc/dressing.nvim',
   lazy = true,
+
   opts = {
     input = {
       enabled = true,
       title_pos = 'left',
       start_in_insert = true,
 
+      -- When true, <Esc> will close the modal
+      insert_only = true,
+
       -- Set to `false` to disable
       mappings = {
-        n = {
+        --- @NOTE: normal mode commented bcuz insert_only is true
+        --[[ n = {
           ['<Esc>'] = 'Close',
           ['<cr>'] = 'Confirm',
-        },
+        }, ]]
+
         i = {
-          ['<Esc>'] = 'Close',
+          --['<Esc>'] = 'Close',
           ['<cr>'] = 'Confirm',
           ['<C-k>'] = 'HistoryPrev',
           ['<C-j>'] = 'HistoryNext',
+          ['<C-c>'] = false, -- disable <C-c> for close
         },
       },
     }
