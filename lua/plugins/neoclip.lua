9241d875e625dbe930399cb40ff39ded51a288cb
Eliezer Ambion <ambion@theoriamedical.com>
2023-07-09 12:53:46 +0800

    Add neoclip for saved yanks

--- lua/plugins/neoclip.lua
+++ lua/plugins/neoclip.lua
@@ -1,53 +1,46 @@
 return {
   'AckslD/nvim-neoclip.lua',
-  lazy = true,
-
-  dependencies = {
-    { 'kkharji/sqlite.lua', module = 'sqlite' },
-    -- { 'nvim-telescope/telescope.nvim' },
-  },
+  lazy = false,
 
   keys = {
     { '<leader>fy', '<cmd>Telescope neoclip<cr>' }
   },
 
-  opts = {
-    preview = true,
+  config = function()
+    require('telescope').load_extension('neoclip')
 
-    keys = {
-      telescope = {
-        i = {
-          paste_behind = false, -- to disable <C-k> as keymap
+    -- needed to prevent storing white spaces
+    local function is_whitespace(line)
+      return vim.fn.match(line, [[^\s*$]]) ~= -1
+    end
+
+    -- needed to prevent storing white spaces
+    local function all(tbl, check)
+      for _, entry in ipairs(tbl) do
+        if not check(entry) then
+          return false
+        end
+      end
+      return true
+    end
+
+    require('neoclip').setup({
+      preview = true,
+
+      keys = {
+        telescope = {
+          i = {
+            paste = '<cr>',
+            paste_behind = false, -- to disable <C-k> as keymap
+          }
         }
-      }
-    },
-  },
+      },
 
-  --init = function()
-  --  require('telescope').load_extension('neoclip')
-  --end
+      -- prevent storing white spaces
+      filter = function(data)
+        return not all(data.event.regcontents, is_whitespace)
+      end
 
-  -- config = function()
-  -- require('telescope').load_extension('neoclip')
-  --
-  --    -- prevent storing white spaces
-  --    local function is_whitespace(line)
-  --      return vim.fn.match(line, [[^\s*$]]) ~= -1
-  --    end
-  --
-  --    -- prevent storing white spaces
-  --    local function all(tbl, check)
-  --      for _, entry in ipairs(tbl) do
-  --        if not check(entry) then
-  --          return false
-  --        end
-  --      end
-  --      return true
-  --    end
-  --
-  --    -- prevent storing white spaces
-  --    filter = function(data)
-  --      return not all(data.event.regcontents, is_whitespace)
-  --    end
-  --  end
+    })
+  end
 }
