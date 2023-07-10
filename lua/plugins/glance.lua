b48cbe65180440bd018123b4a885f4db8e3b1c18
Eliezer Ambion <ambion@theoriamedical.com>
2023-07-11 03:12:53 +0800

    Tweak glance nvim

--- lua/plugins/glance.lua
+++ lua/plugins/glance.lua
@@ -2,17 +2,23 @@ return {
   'dnlhc/glance.nvim',
   lazy = true,
 
-  --[[ keys = {
-    {'<leader>gd', '<cmd>Glance definitions<cr>' , desc = 'Glance Definition'},
-    {'<leader>gr', '<cmd>Glance references<cr>' , desc = 'Glance Reference'},
-    {'<leader>gt', '<cmd>Glance type_definitions<cr>' , desc = 'Glance Type_Definition'},
-    {'<leader>gi', '<cmd>Glance implementations<cr>' , desc = 'Glance Implementation'},
-  }, ]]
+  keys = {
+    { '<leader>gd', '<cmd>Glance definitions<cr>',      desc = 'Glance Definition' },
+    { '<leader>gr', '<cmd>Glance references<cr>',       desc = 'Glance Reference' },
+    { '<leader>gt', '<cmd>Glance type_definitions<cr>', desc = 'Glance Type_Definition' },
+    { '<leader>gi', '<cmd>Glance implementations<cr>',  desc = 'Glance Implementation' },
+  },
 
   opts = {
     theme = {
       enable = true,
-      mode = 'darken'
+      mode = 'auto'
     },
+
+    detached = false,
+
+    border = {
+      enable = true,
+    }
   }
 }
