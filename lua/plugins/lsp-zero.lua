c87c606fb0544499bfa12894ef48337ccff3ed96
Eliezer Ambion <ambion@theoriamedical.com>
2023-07-14 03:29:58 +0800

    Add highlight on first candidate of autocomplete

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -82,16 +82,21 @@ return {
     vim.api.nvim_set_hl(0, "MyCursorLine", { bg = rosepine.pine, fg = rosepine.text, bold = true, italic = true })
 
     cmp.setup {
+      completion = {
+        completeopt = 'menu,menuone,noinsert', -- highlight the first candidate in autocomplete
+      },
+
       window = {
         completion = {
           border = 'rounded',
-          winhighlight = "Normal:MyPmenu,CursorLine:MyCursorLine", -- this will set rose-pine border as well
+          winhighlight = "Normal:MyPmenu,CursorLine:MyCursorLine", -- adding this will set rose-pine border as well
         },
         documentation = {
           border = 'rounded',
           winhighlight = "Normal:MyPmenu",
         },
       },
+
       -- sources for autocompletion
       sources = {
         { name = 'nvim_lsp' }, -- lsp
@@ -106,15 +111,15 @@ return {
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-u>'] = cmp.mapping.scroll_docs(4),
         ["<C-e>"] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm { select = true },
+        ['<CR>'] = cmp.mapping.confirm { select = true }, -- select first candidate in autocomplete upon enter
       },
 
-      formatting = {
+      --[[ formatting = {
         format = lspkind.cmp_format({
           maxwidth = 60,
           ellipsis_char = "...",
         }),
-      },
+      }, ]]
     }
   end,
 }
